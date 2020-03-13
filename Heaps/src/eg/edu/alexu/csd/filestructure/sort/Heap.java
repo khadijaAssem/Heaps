70396503314abfb06a3bf6adfc5206e3d5ffc684
khadijaAssem <42361430+khadijaAssem@users.noreply.github.com>
2020-03-13 04:58:51 +0200

    Add files via upload

--- Heaps/src/eg/edu/alexu/csd/filestructure/sort/Heap.java
+++ Heaps/src/eg/edu/alexu/csd/filestructure/sort/Heap.java
@@ -3,39 +3,87 @@ package eg.edu.alexu.csd.filestructure.sort;
 import java.util.Collection;
 
 public class Heap implements IHeap {
-    private int MAX = 5;
-    INode root;
-    int size =0;
+    private int MAX = 20000000;
+    int size =1;
     Node[] heap;
+    Node last;
     public Heap(){
         heap = new Node[MAX];
-        root = new Node(heap,0);
-        size++;
     }
 
     @Override
     public INode getRoot() {
-        return root;
+        return heap[1];
     }
 
     @Override
     public int size() {
-        return size;
+        return size-1;
     }
 
     @Override
     public void heapify(INode node) {
+        boolean flag = true;
+        while(flag) {
+            try {
+                while (node.getValue().compareTo(node.getParent().getValue()) > 0) {
+//            if (node.getValue().compareTo(node.getParent().getValue()) > 0) {
+                    Comparable temp = node.getValue();
+                    node.setValue(node.getParent().getValue());
+                    node.getParent().setValue(temp);
+                    node = node.getParent();
+//                continue;
+//            }
+                }
+            }
+            catch (Exception e){}
 
+            try {
+//                System.out.println("HERE");
+                if (size % 2 != 0 && node.getValue().compareTo(node.getParent().getLeftChild().getValue()) > 0) {
+                    Comparable temp = node.getValue();
+                    node.setValue(node.getParent().getLeftChild().getValue());
+                    node.getParent().getLeftChild().setValue(temp);
+                    continue;
+                }
+            }
+            catch (Exception e){/*System.out.println("EXCEPTION");*/}
+            flag =false;
+        }
+//       else if (size%2==0&&size!=2&&node.getValue().compareTo(node.getParent().getRightChild().getValue())<0){
+//           Comparable temp = node.getValue();
+//           node.setValue(node.getParent().getRightChild().getValue());
+//           node.getParent().setValue(temp);
+//       }
     }
 
     @Override
     public Comparable extract() {
-        return null;
+        Comparable x = heap[size-1].getValue();
+        heap[size-1].setValue(null);
+        heap[size-1] = null;
+        size--;
+        return x;
     }
 
     @Override
     public void insert(Comparable element) {
-
+        if (element==null) return;
+        INode nNode = new Node(heap,size);
+        nNode.setValue(element);
+        if(size == 1){
+            System.out.println("ROOT "+element);
+            heap[size++] = (Node) nNode;
+            return;
+        }
+        heap[size++] = (Node) nNode;
+        heapify(nNode);
+//        System.out.println("Size "+(size-1));
+//        System.out.println("Element "+element);
+//        for(int i=1;i<size;i++){
+//            System.out.println(heap[i].getValue());
+//        }
+//        size++;
     }
 
     @Override
--- Heaps/src/eg/edu/alexu/csd/filestructure/sort/Node.java
+++ Heaps/src/eg/edu/alexu/csd/filestructure/sort/Node.java
@@ -1,26 +1,33 @@
 package eg.edu.alexu.csd.filestructure.sort;
 
-public class Node implements INode{
+import java.util.Random;
+
+public class Node implements INode {
     Node[] arr;
     Comparable val;
     int index;
-    public Node(Node[] arr,int index){
+
+    public Node(Node[] arr, int index) {
         this.arr = arr;
         this.index = index;
     }
+
     @Override
     public INode getLeftChild() {
-        return arr[index*2];
+//        System.out.println("Left Child "+index*2);
+        return arr[index * 2];
     }
 
     @Override
     public INode getRightChild() {
-        return arr[index*2+1];
+        return arr[index * 2 + 1];
     }
 
     @Override
     public INode getParent() {
-        return arr[index/2];
+//        if(index==1)
+//            return
+        return arr[index / 2];
     }
 
     @Override
@@ -32,4 +39,20 @@ public class Node implements INode{
     public void setValue(Comparable value) {
         this.val = value;
     }
+
+//    public static void main(String[] args) {
+//        IHeap<Integer> heap = (IHeap) TestRunner.getImplementationInstanceForInterface(IHeap.class);
+//        INode root = null;
+//        Integer max = 0;
+//
+//        for (int i = 0; i < 10; ++i) {
+//            Random r = new Random();
+//            int val = r.nextInt(200);
+//            heap.insert(val);
+//            max = Math.max(max, val);
+//        }
+//
+//        root = heap.getRoot();
+//        System.out.println(max + " " + root.getValue());
+//    }
 }
