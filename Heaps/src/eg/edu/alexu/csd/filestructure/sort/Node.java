1fe6d9123643a186edc43746a78bcdb3ded85a9a
norhanmagdi <58911005+norhanmagdi@users.noreply.github.com>
2020-03-15 18:31:09 +0200

    Add files via upload

--- Heaps/src/eg/edu/alexu/csd/filestructure/sort/Node.java
+++ Heaps/src/eg/edu/alexu/csd/filestructure/sort/Node.java
@@ -1,72 +1,34 @@
 package eg.edu.alexu.csd.filestructure.sort;
 
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Random;
+import java.util.ArrayList;
 
-public class Node implements INode {
-    Node[] arr;
-    Comparable val;
-    int index;
+class Node<T extends Comparable<T>> implements INode {
+	private Integer index;
+	private int [] array;
 
-    public Node(Node[] arr, int index) {
-        this.arr = arr;
-        this.index = index;
-    }
 
-    @Override
-    public INode getLeftChild() {
-//        System.out.println("Left Child "+index*2);
-        return arr[index * 2];
-    }
+	@Override
+	public INode getLeftChild() {
+		return null;
+	}
 
-    @Override
-    public INode getRightChild() {
-        return arr[index * 2 + 1];
-    }
+	@Override
+	public INode getRightChild() {
+		return null;
+	}
 
-    @Override
-    public INode getParent() {
-//        if(index==1)
-//            return
-        return arr[index / 2];
-    }
+	@Override
+	public INode getParent() {
+		return null;
+	}
 
-    @Override
-    public Comparable getValue() {
-        return val;
-    }
+	@Override
+	public Comparable getValue() {
+		return null;
+	}
 
-    @Override
-    public void setValue(Comparable value) {
-        this.val = value;
-    }
+	@Override
+	public void setValue(Comparable value) {
 
-    public static void main(String[] args) {
-        IHeap heap = (IHeap)TestRunner.getImplementationInstanceForInterface(IHeap.class);
-
-        try {
-            PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-            Random r = new Random();
-            Random pick = new Random();
-
-            for(int i = 0; i < 10; ++i) {
-                int numToPick = pick.nextInt(200);
-                int val = r.nextInt(200);
-                if (numToPick % 4 == 0) {
-                    if (!pq.isEmpty()) {
-                        System.out.println("O/P 1 "+pq.poll()+" "+heap.extract());
-                    } else {
-                        System.out.println("O/P 2 "+0L+" "+(long)heap.size());
-                    }
-                } else {
-                    pq.add(val);
-                    heap.insert(val);
-                }
-            }
-        } catch (Throwable var8) {
-            TestRunner.fail("Fail in heap", var8);
-        }
-
-    }
+	}
 }
